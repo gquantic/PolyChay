5970e85f13c8b7a781e1da86074679254a46bbaf
Gadji12 <gadjisila228@gmail.com>
2022-01-12 22:56:39 +0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,89 @@
 @extends('layouts.meta')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<body style="background: url('img/covermute.png');background-size: cover;background-repeat: no-repeat;">
+<form action="{{ route('login') }}" method="post">
+    @csrf
+    <div class="in_form">
+        <h2 class="mt-3">Авторизация</h2>
+        <input type="email" class="mt-2 @error('email') is-invalid @enderror"  placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        <input type="password" placeholder="Пароль" name="password" class="form-control @error('password') is-invalid @enderror" required autocomplete="current-password"><br>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <input type="submit" value="Войти" name="auth">
+        <div class="row mt-3">
+            <div class="col-md-6">
+                <a href="forgerPassword.php" style="margin-top: 10px;">Забыли пароль?</a>
+            </div>
+            <div class="col-md-6 text-right">
+                <a href="employer/index.php" style="margin-top: 10px;">Я оффициант</a>
+            </div>
+            <div class="col-md-12">
+                <a href="http://polychay.ru" style="margin-top: 10px;">Регистрация</a>
+            </div>
+            <div class="col-md-12">
+                <p style="color:#d20404;/*position: absolute;margin-top: 240px;*/" class="error"></p>
             </div>
         </div>
     </div>
-</div>
+</form>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    form {
+        width: 400px;
+        height: 300px;
+        padding: 5px;
+        margin: 0 auto;
+        font-family: 'Gilroy Bold';
+        background: #fff;
+        border-radius: 4px;
+        margin-top: 25px;
+        background: url('img/bglines.png');
+        border-radius: 14px;
+        background-size: cover;
+    }
+    form .in_form {
+        display: flex;
+        flex-direction: column;
+        background: #fff;
+        height: 100%;
+        width: 100%;
+        border-radius: 14px;
+        padding: 5px 20px;
+    }
+    form input[type="text"], form input[type="password"], form input[type="email"] {
+        height: 35px;
+        margin-bottom: 2px;
+        background: rgba(0,0,0,.02);
+        padding: 2px 10px;
+        border-radius: 4px;
+        font-size: 14px;
+    }
+    form input[type="submit"] {
+        height: 40px;
+        font-size: 16px;
+        font-family: "Gilroy";
+        border-radius: 4px;
+        cursor: pointer;
+    }
+</style>
+</body>
+</html>
+
 @endsection
